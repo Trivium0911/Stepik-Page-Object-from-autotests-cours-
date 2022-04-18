fb4f95c74d3b16fcfb845ecc0e741ce0079f56ca
Trivium0911 <triv_1993@mail.ru>
2022-04-18 13:50:29 +0300

    added test_4,3_step_6.py with tests for lesson 4.3 step 6

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import MainPageLocators, ProductPageLocators
 
 
 class MainPage(BasePage):
@@ -14,3 +14,4 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
--- pages/product_page.py
+++ pages/product_page.py
@@ -25,3 +25,11 @@ class ProductPage(BasePage):
         message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert product_price in message_basket_total, "No product price in the message"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_dissapered_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
