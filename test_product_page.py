ddcac9dd06c6f76fe4dbee7eacd9abdd23afa58e
Trivium0911 <triv_1993@mail.ru>
2022-04-24 15:29:16 +0300

    added comments

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,6 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-
 class TestGuestAddToBasketFromProductPage:
 
     @pytest.mark.need_review
@@ -25,6 +24,15 @@ class TestGuestAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+
+@pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks = pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(self, browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.press_button_add_to_basket()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
 
 @pytest.mark.xfail(reason="Success message is presented, but should not be")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
